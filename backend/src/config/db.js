d7cf8216eaed003fc3030af4da8fa03ea84157f1
GabrielPMorgado <gabrielmorgado204@gmail.com>
2026-03-03 07:32:57 -0300

    main

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'sistema_residencial',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_PASSWORD || '123456',
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
